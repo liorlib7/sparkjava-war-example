f27830ad84a8f9fe204368d441f256aaaeedb038
liorlib7 <46499876+liorlib7@users.noreply.github.com>
2020-04-20 21:36:51 +0300

    Update httpcheck.sh

--- httpcheck.sh
+++ httpcheck.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
 export test=$(curl -LI localhost:8090/sparkjava-hello-world-1.0/hello  -o /dev/null -w '%{http_code}\n' -s)
-
-
 if [[ $test == 2* ]]
 then
 	docker rm -f testi
@@ -10,4 +7,3 @@ else
 	echo "HTTP Error!" 1>&2
 	exit 1
 fi
-
