7299ac15a32390314b46359f02075021b95b616e
kfirlevin <54764151+kfirlevin@users.noreply.github.com>
2020-04-21 11:50:56 +0300

    Update httpcheck.sh

--- httpcheck.sh
+++ httpcheck.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+
 status=$(curl -LI localhost:8090/sparkjava-hello-world-1.0/hello  -o /dev/null -w '%{http_code}\n' -s)
-if [ "$status" != "2"* ]
-then 
-    echo "URL is not working"
-   	exit 1
+
+codeNum=$((status))
+
+if [ $codeNum -ge 300 ]
+then
+        echo "ERROR"
+        exit 1
 fi
-echo "$status"
+
+echo $codeNum
