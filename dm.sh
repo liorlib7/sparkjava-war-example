6019b557ece84fdbb73da01fc34705822cca5c78
liorlib7 <46499876+liorlib7@users.noreply.github.com>
2020-04-21 12:12:39 +0300

    Update dm.sh

--- dm.sh
+++ dm.sh
@@ -1,1 +1,9 @@
-
+#!/bin/bash
+status=$(curl -LI localhost:8090/sparkjava-hello-world-1.0/hello  -o /dev/null -w '%{http_code}\n' -s)
+codeNum=$((status))
+if [ $codeNum -ge 300 ]
+then
+        echo "ERROR"
+        exit 1
+fi
+echo $codeNum
