c9cb51c677c094292807a80dd0a176ce874737ce
liorlib7 <46499876+liorlib7@users.noreply.github.com>
2020-04-21 10:41:36 +0300

    Update httpcheck.sh

--- httpcheck.sh
+++ httpcheck.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 status=$(curl -LI localhost:8090/sparkjava-hello-world-1.0/hello  -o /dev/null -w '%{http_code}\n' -s)
-  if [ "$status" = "2"* ]; then 
+if [ "$status" == "2"* ]
+then 
     echo " URL is working fine"
     break
     #docker rm -f www
-  else
+else
     echo "URL is not working"
     exit 1
 fi
+echo "$status"
