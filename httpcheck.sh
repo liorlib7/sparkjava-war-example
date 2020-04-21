b0d2a9106b4e51f042b00e3137b19dfb35758e20
liorlib7 <46499876+liorlib7@users.noreply.github.com>
2020-04-21 11:17:39 +0300

    Update httpcheck.sh

--- httpcheck.sh
+++ httpcheck.sh
@@ -3,6 +3,6 @@ status=$(curl -LI localhost:8090/sparkjava-hello-world-1.0/hello  -o /dev/null -
 if [ "$status" != "2"* ]
 then 
     echo "URL is not working"
-    exit 1
+   	exit 1
 fi
 echo "$status"
